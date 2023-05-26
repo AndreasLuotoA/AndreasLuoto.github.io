4eee977ea43cebc430c17d1ce9bc4d65d1c00194
Andreas Luoto <andreas.luoto@adafy.com>
2023-05-26 11:23:28 +0300

    updated darudefiles and added text file

--- www-page4/darudeScript.js
+++ www-page4/darudeScript.js
@@ -1,30 +1,29 @@
-function getAlbums() {
-    let url = "https://itunes.apple.com/lookup/?id=1472380750&entity=album";
-    $.get(url, function(data, status){
-        if (status != "success") {
-            console.log(`Error getting ${url}`);
-            return;
-        }
-        const albums = JSON.parse(data);
-        
-        for (var key in albums){
-            for (var key1 in albums[key]) {
-                addCard(albums[key][key1]);
+
+$(document).ready(function() {
+    const container = $('.js-results');
+
+    $.get("../darudeFile.txt", function(data, status) {
+        const { resultCount, results } = JSON.parse(data);
+
+        const row = $('<div>').addClass('row row-cols-1 row-cols-md-4 g-4');
+
+        $.each(results, function(index, result) {
+            const { collectionName, artworkUrl60, artworkUrl100 } = result;
+
+            if (artworkUrl100) {
+                const col = $('<div>').addClass('col');
+                const img = $('<img>').addClass('img-fluid');
+                const albumName = $('<div>').text(collectionName);
+
+                img.attr('src', artworkUrl100.replace("100x100bb", "1024x1024bb"));
+                img.addClass('m-1');
+                
+                col.append(img);
+                col.append(albumName);
+                row.append(col);
             }
-        }
+        });
+
+        container.append(row);
     });
-
-}
-
-function addCard(album) {
-    if (album.collectionType != "Album") return;
-    let card = "<div class=\"col-12 col-lg-3 \">" +
-               "<div class=\"card h-100\" >" +
-                   `<img class=\"card-img-top\" src=\"${album.artworkUrl100.replace("100x100bb", "1024x1024bb")}\" alt=\"${album.collectionName}\">` +
-                   "<div class=\"card-body\">" +
-                       `<h5 class=\"card-title\">${album.collectionName}</h5>` +
-                   "</div>" +
-               "</div>"+ 
-           "</div> ";
-    $("#albumrow").append(card);
-}
+});
